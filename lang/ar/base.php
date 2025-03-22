32ad4170bd54016d8d0ac18fe7bfa1288df753b4
Yacine REZGUI <yacine.rezgui@gmail.com>
2025-03-22 11:46:45 +0100

    added arabic language

---
+++ lang/ar/base.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Base Barcode Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during Barcode for
+    | various messages that we need to display to the user.
+    |
+    */
+
+    "validation" => [
+        "barcode_not_found" => "لم يتم العثور على الباركود.",
+    ],
+
+    "messages" => [
+        "created" => "تم إنشاء الباركود بنجاح.",
+        "updated" => "تم تحديث الباركود بنجاح.",
+        "deleted" => "تم حذف الباركود بنجاح.",
+        "all_deleted" => "تم حذف جميع الباركود بنجاح.",
+    ],
+
+];
