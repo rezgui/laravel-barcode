358b1a4de99ebb7b1779bfae86659bd0cf89f251
Yacine REZGUI <yacine.rezgui@gmail.com>
2025-03-22 11:41:30 +0100

    Create base.php
    Create base.php for French language.

---
+++ lang/fr/base.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Base Barcode Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during Barcode for
+    | various messages that we need to display to the user.
+    |
+    */
+
+    "validation" => [
+        "barcode_not_found" => "Le code-barres n'a pas été trouvé.",
+    ],
+
+    "messages" => [
+        "created" => "Le code-barres a été créé avec succès.",
+        "updated" => "Le code-barres a été mis à jour avec succès.",
+        "deleted" => "Le code-barres a été supprimé avec succès.",
+        "all_deleted" => "Tous les code-barres ont été supprimés avec succès.",
+    ],
+
+];
